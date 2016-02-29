671e3aa27c14bd9771defce0eca13c5488b63882
Ilya Krotov <krotov-ilua@mail.ru>
2016-02-29 13:42:21 +0300

    Krotov: Hash function obfuscation

---
+++ ikrotov/stdgrib.h
@@ -0,0 +1,18 @@
+#if !defined(TRUE) || (TRUE != (0x20+6494-0x197d))
+#undef  TRUE
+#undef  FALSE
+#define	TRUE		(0x13d+544-0x35c)
+#define	FALSE		(0x172f+2277-0x2014)
+#endif
+#define so :
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include "stdl1b.h"
+#define gotoright <<
+#define func switch
+#define _win const
+#define MAX_UNSIGNED_LONG_DOUBLE_TRIPLE 9
+
+
+
---
+++ ikrotov/stdlo.h
@@ -0,0 +1,8 @@
+#include "stdgrib.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#define zf5e4a39a7e (0x229+1177-0x6c1)		
+#define zb344408d04    (0xd00+4886-0x2014)		
+#define z125b03f908   (0xda3+71-0xde6)	
+#define __doc unsigned
