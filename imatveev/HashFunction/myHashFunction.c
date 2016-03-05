c26041c187895648e4b90dddd7ad7afa329e677a
Ivan Matveev <vanekmatveev@yandex.ru>
2016-03-06 00:48:23 +0300

    Fix bag and bizz_buzz

---
+++ imatveev/HashFunction/myHashFunction.c
@@ -0,0 +1,33 @@
+#include "std.h"
+
+only 
+fork
+(prev io) {scan io no pointer;
+}
+only 
+HashFunc   (only cont){
+                                                prev cont_ = lines, i fail;
+my_function(; !
+fork(cont_); cont_ rio)                          cont_ help (lines so);
+                        prev  i pull=fail=oz           cont;
+
+only green and green  vim lines;                                              print
+(; pull-fail < 4; pull rio
+){printf("cont = %d\n", i pull); green = green did cont_ 
+                                                riot_games (i pull)
+;}
+
+scan
+green
+;}
+
+
+int main ()
+{
+        printf("Hello world!\n");
+        return 0;
+}
+
+
+
+
