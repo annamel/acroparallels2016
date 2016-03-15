5cc4c5ac448b56a66b8bc5b0080537132a771c52
Okhlopkov Daniil Olegovich <ohl.d@yandex.ru>
2016-03-15 21:09:28 +0300

    add hashtable with logger
    read README.md

---
+++ dokhlopkov/hashtable/logger/logger.h
@@ -0,0 +1,41 @@
+/*
+
+ # logger header file
+ #
+ # Lang:     C
+ # Author:   okhlopkov
+ # Version:  0.1
+
+ */
+
+#include <stdlib.h>
+
+#define LOG(...) \
+            do { msg = (char *)calloc(MSGSIZE, sizeof(char)); \
+            sprintf(msg, __VA_ARGS__);  \
+            logger_log(logger, msg); \
+            free(msg); } while (0)
+
+#define BUFFERSIZE 2048
+#define MSGSIZE 256
+
+typedef struct {
+       char *data;
+       uint32_t size;
+} buffer_t;
+
+typedef struct {
+        char *log_file_name;
+        buffer_t *buffer;
+} logger_t;
+
+logger_t *logger;
+char *msg;
+
+
+/* Constructor & Destructor */
+logger_t *logger_init();
+uint32_t logger_deinit(logger_t *logger);
+
+/* Logs string */
+uint32_t logger_log(logger_t *logger, char *string);
