5f5267586a8473af0135c0e9ca2d1ae970d4ac2c
Ivan Matveev <vanekmatveev@yandex.ru>
2016-03-19 19:22:05 +0300

    make singleton in logger

--- imatveev/logger/main.c
+++ imatveev/logger/main.c
@@ -5,8 +5,13 @@ int main () {
         #ifdef DEBUG
         printf("DEBUG\n");
         #endif
-        log_init();
-        log_error(TRUE, ERROR, "GAME OVER");
-        log_end();
+        set_log_level(warning);
+        set_log_file("my_file");
+        log_error(debug,   "GAME OVER_1");
+        log_error(info,    "GAME OVER_2");
+        log_error(warning, "GAME OVER_3");
+        log_error(error,   "GAME OVER_4");
+        log_error(fatal,   "GAME OVER_5");
+        log_error(warning, "GAME OVER_6");
         return 0;
 }
