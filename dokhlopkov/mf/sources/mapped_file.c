c419facbe905be1ca1939a01c8d9ccdb7a5759e1
ohld <ohl.d@yandex.ru>
2016-04-20 22:59:10 +0300

    codestyle correction

--- dokhlopkov/mf/sources/mapped_file.c
+++ dokhlopkov/mf/sources/mapped_file.c
@@ -10,69 +10,69 @@
 
 
 mf_handle_t mf_open(const char* name, size_t max_memory) {
-  if (name == NULL) return MF_OPEN_FAILED;
-  mf_handle_t mf;
+    if (name == NULL) return MF_OPEN_FAILED;
+    mf_handle_t mf;
 
-  int fd = open(name, O_RDWR | O_CREAT, S_IWRITE | S_IREAD);
-  if (fd == -1) return MF_OPEN_FAILED;
+    int fd = open(name, O_RDWR | O_CREAT, S_IWRITE | S_IREAD);
+    if (fd == -1) return MF_OPEN_FAILED;
 
-  cpool_t *cpool = NULL;
-  int err = cpool_construct(max_memory, fd, &cpool);
-  if (err) return MF_OPEN_FAILED;
+    cpool_t *cpool = NULL;
+    int err = cpool_construct(max_memory, fd, &cpool);
+    if (err) return MF_OPEN_FAILED;
 
-  mf = (void *)cpool;
-  return mf;
+    mf = (void *)cpool;
+    return mf;
 }
 
 int mf_close(mf_handle_t mf) {
-  if (!mf) return EAGAIN;
-  return cpool_destruct((cpool_t **)&mf);
+    if (!mf) return EAGAIN;
+    return cpool_destruct((cpool_t **)&mf);
 }
 
 ssize_t mf_read(mf_handle_t mf, off_t offset, size_t size, void *buf) {
-  cpool_t *cpool = (cpool_t *)mf;
-  chunk_t *ch = NULL;
-  ssize_t read_bytes = 0;
-  int err = ch_find(cpool, offset, size, &ch);
+    cpool_t *cpool = (cpool_t *)mf;
+    chunk_t *ch = NULL;
+    ssize_t read_bytes = 0;
+    int err = ch_find(cpool, offset, size, &ch);
 
-  void *src = NULL;
-  switch(err) {
+    void *src = NULL;
+    switch(err) {
     case 0:
-      err = ch_get_mem(ch, offset, &src);
-      if (err) return err;
-      memcpy(buf, src, size);
-      read_bytes = size;
-      break;
+        err = ch_get_mem(ch, offset, &src);
+        if (err) return err;
+        memcpy(buf, src, size);
+        read_bytes = size;
+        break;
     case ENODATA:
-      read_bytes = pread(cpool_fd(cpool), buf, size, offset);
-      if (read_bytes == -1) return errno;
-      break;
+        read_bytes = pread(cpool_fd(cpool), buf, size, offset);
+        if (read_bytes == -1) return errno;
+        break;
     default:
-      return err;
-  }
-  return read_bytes;
+        return err;
+    }
+    return read_bytes;
 }
 
 ssize_t mf_write(mf_handle_t mf, off_t offset, size_t size, void *buf) {
-  cpool_t *cpool = (cpool_t *)mf;
-  chunk_t *ch = NULL;
-  ssize_t written_bytes = 0;
-  int err = ch_find(cpool, offset, size, &ch);
+    cpool_t *cpool = (cpool_t *)mf;
+    chunk_t *ch = NULL;
+    ssize_t written_bytes = 0;
+    int err = ch_find(cpool, offset, size, &ch);
 
-  void *dst = NULL;
-  switch(err) {
+    void *dst = NULL;
+    switch(err) {
     case 0:
-      err = ch_get_mem(ch, offset, &dst);
-      if (err) return err;
-      memcpy(dst, buf, size);
+        err = ch_get_mem(ch, offset, &dst);
+        if (err) return err;
+        memcpy(dst, buf, size);
     case ENODATA:
-      written_bytes = pwrite(cpool_fd(cpool), buf, size, offset);
-      if (written_bytes == -1) return errno;
-      break;
+        written_bytes = pwrite(cpool_fd(cpool), buf, size, offset);
+        if (written_bytes == -1) return errno;
+        break;
     default:
-      return err;
-  }
-  return 0;
+        return err;
+    }
+    return 0;
 }
 
 mf_mapmem_t *mf_map(mf_handle_t mf, off_t offset, size_t size) {
