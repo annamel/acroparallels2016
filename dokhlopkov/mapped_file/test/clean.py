43b376eb5900a9f234438046d0a0bf81f01de7b2
ohld <ohl.d@yandex.ru>
2016-04-23 00:15:16 +0300

    make tests more suitable for group testing

--- dokhlopkov/mapped_file/test/clean.py
+++ dokhlopkov/mapped_file/test/clean.py
@@ -2,5 +2,7 @@ import os
 
 if os.path.isfile("big"):
     os.remove("big")
+if os.path.isfile("small"):
+    os.remove("small")
 if os.path.isfile("temp"):
     os.remove("temp")
--- dokhlopkov/mapped_file/test/prepare.py
+++ dokhlopkov/mapped_file/test/prepare.py
@@ -18,3 +18,10 @@ big.write("\n")
 big.write("4" * (7))
 big.write("\n")
 big.close()
+
+petite = 10 * 4
+
+small = open('small', 'w')
+small.write("1" * petite)
+small.write("\n")
+small.close()
