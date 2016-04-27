d2856b79b1f8edf69b8bb886bc86fb8065cfdc39
Alexey Ivanov <eviom17@gmail.com>
2016-04-27 21:47:33 +0300

    Added read, write, implemented small-file optimization

--- aivanov/mapped_file/MappedFile.cpp
+++ aivanov/mapped_file/MappedFile.cpp
@@ -4,16 +4,40 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <limits>
+#include <cstring>
 
 CMappedFile::CMappedFile(const char* fileName) :
 	desc_(-1),
-	root_(0, std::max(2000l, std::numeric_limits<off_t>::max()))
+	root_(0, 0),
+	entireFile_(NULL),
+	cache_(NULL),
+	size_(0)
 {
 	desc_ = open(fileName, O_RDWR | O_CREAT, 0755);
+	
+	if (isValid())
+	{
+		struct stat buf = {};
+		fstat(desc_, &buf);
+		size_ = buf.st_size;
+		root_.size_ = size_;
+		
+		if (size_ && size_ <= MAX_ENTIRELY_MAPPED_SIZE)
+		{
+			entireFile_ = map(0, size_, NULL);
+			assert(entireFile_);
+		}
+	}
 }
 
 CMappedFile::~CMappedFile()
 {
+	if (entireFile_)
+		entireFile_->unmap();
+		
+	if (cache_)
+		cache_->unmap();
+		
 	close(desc_);
 }
 
@@ -22,9 +46,10 @@ CFileRegion* CMappedFile::map(off_t offset, off_t size, void** address)
 	long pageSize = sysconf(_SC_PAGE_SIZE);
 	off_t properOffset = (offset / pageSize) * pageSize;
 	size_t properSize = ((size + (offset - properOffset) + pageSize - 1) / pageSize) * pageSize;
-
+	properSize = std::min(properSize, size_t(size_ - properOffset));
 	CFileRegion* newRegion = new CFileRegion(properOffset, properSize);
 	CFileRegion* region = root_.takeChild(newRegion);
+	assert(region->doesInclude(newRegion));
 	region->addReference();
 	assert(region->references_ > 0);
 	
@@ -41,22 +66,65 @@ CFileRegion* CMappedFile::map(off_t offset, off_t size, void** address)
 	else
 		delete newRegion;
 	
-	*address = region->address_ + (offset - region->offset_);
+	if (address)
+		*address = region->address_ + (offset - region->offset_);
+		
 	return region;
 }
 
-ssize_t CMappedFile::read(off_t offset, size_t size, void *buf)
+ssize_t CMappedFile::fileCopy_(off_t offset, size_t size, uint8_t* to, const uint8_t* from)
 {
-	lseek(desc_, offset, SEEK_SET);
-	ssize_t status = ::read(desc_, buf, size);
-	return status;
+	assert(!to || !from);
+	assert(to || from);
+	
+	size = std::min(size, size_t(std::max(size_ - offset, off_t(0))));
+	
+	size_t bytesProcessed = 0;
+	while (size)
+	{
+		CFileRegion* region = root_.maxAt(offset);
+				
+		if (!region)
+		{
+			if (cache_)
+			{
+				cache_->unmap();
+				
+				if (!*cache_)
+					delete cache_;
+			}
+			
+			region = cache_ = map(offset, CACHE_SIZE_PAGES * sysconf(_SC_PAGE_SIZE), NULL);
+		}
+		
+		assert(region);
+		
+		off_t offsetAtRegion = offset - region->offset_;
+		void* mappedAddress = region->address_ + offsetAtRegion;
+		size_t mappedSize = region->size_ - offsetAtRegion;
+		mappedSize = std::min(mappedSize, size);
+		
+		if (to)
+			memcpy(to + bytesProcessed, mappedAddress, mappedSize);
+		else
+			memcpy(mappedAddress, from + bytesProcessed, mappedSize);
+		
+		bytesProcessed += mappedSize;
+		offset += mappedSize;
+		size -= mappedSize;
+	}
+	
+	return bytesProcessed;
 }
 
-ssize_t CMappedFile::write(off_t offset, size_t size, const void *buf)
+ssize_t CMappedFile::read(off_t offset, size_t size, uint8_t* buf)
 {
-	lseek(desc_, offset, SEEK_SET);
-	ssize_t status = ::write(desc_, buf, size);
-	return status;
+	return fileCopy_(offset, size, buf, NULL);
+}
+
+ssize_t CMappedFile::write(off_t offset, size_t size, const uint8_t* buf)
+{
+	return fileCopy_(offset, size, NULL, buf);
 }
 
 bool CMappedFile::isValid()
@@ -66,8 +134,6 @@ bool CMappedFile::isValid()
 
 off_t CMappedFile::getSize()
 {
-	struct stat buf;
-	fstat(desc_, &buf);
-	return buf.st_size;
+	return size_;
 }
 
--- aivanov/mapped_file/MappedFile.h
+++ aivanov/mapped_file/MappedFile.h
@@ -3,6 +3,8 @@
 
 #include "FileRegion.h"
 
+#define MAX_ENTIRELY_MAPPED_SIZE (1 << 27)
+#define CACHE_SIZE_PAGES 2
 
 class CMappedFile
 {
@@ -10,15 +12,19 @@ class CMappedFile
 private:
 	int				desc_;
 	CFileRegion		root_;
-					
+	CFileRegion*	entireFile_;
+	CFileRegion*	cache_;
+	off_t			size_;
+	
+	ssize_t fileCopy_(off_t offset, size_t size, uint8_t* to, const uint8_t* from);
 public:
 	CMappedFile(const char* fileName);
 	~CMappedFile();
 
 	CFileRegion* map(off_t offset, off_t size, void** address);
 	off_t getSize();
-	ssize_t read(off_t offset, size_t size, void *buf);
-	ssize_t write(off_t offset, size_t size, const void *buf);
+	ssize_t read(off_t offset, size_t size, uint8_t* buf);
+	ssize_t write(off_t offset, size_t size, const uint8_t* buf);
 	bool isValid();
 	
 };
