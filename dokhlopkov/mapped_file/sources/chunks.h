5ef186b2a2685f278eb92ee5ab129878fc9bf5e6
ohld <ohl.d@yandex.ru>
2016-04-27 11:17:44 +0300

    fix chunk manager behavior with ch_list

--- dokhlopkov/mapped_file/sources/chunks.h
+++ dokhlopkov/mapped_file/sources/chunks.h
@@ -12,7 +12,7 @@ int ch_release(chunk_t *chunk);
 int ch_find(cpool_t *cpool, off_t offset, size_t size, chunk_t **chunk);
 
 int cpool_construct(size_t max_mem, int fd, cpool_t **cpool);
-int cpool_destruct(cpool_t **cpool);
+int cpool_destruct(cpool_t *cpool);
 int cpool_mem_add(void *ptr, chunk_t *chunk);
 int cpool_mem_get(cpool_t *cpool, void *ptr, chunk_t **chunk);
 int cpool_fd(cpool_t *cpool);
