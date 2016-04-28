fbe38193dda6d3babcc3cb9ed38b58bee6d931f4
aglab2 <denis.kopyrin@phystech.edu>
2016-04-28 15:27:12 +0300

    Added support for jupyter

--- tester/tester.sh
+++ tester/tester.sh
@@ -149,6 +149,5 @@ done
 PWD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 rm -f $PWD/out.txt
 
-if [ $UNAME == "Darwin" ]; then
-	IFS=$SAVEIFS
-fi
+IFS=$SAVEIFS
+jupyter nbconvert --to=html --ExecutePreprocessor.enabled=True $PWD/test_results.ipynb
