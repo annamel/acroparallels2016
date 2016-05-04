177117c49a417a5bfb7b54bca2e9784bddd975fb
Denis Kopyrin <denis.kopyrin@phystech.edu>
2016-05-04 20:31:59 +0300

    Fixed wrong chunk check correctness in rbtree lookup

--- dkopyrin/mapped_file/chunk_manager/chunk_manager.c
+++ dkopyrin/mapped_file/chunk_manager/chunk_manager.c
@@ -103,8 +103,8 @@ ssize_t chunk_manager_gen_chunk (struct chunk_manager *cm, off_t offset, size_t 
 	LOG(DEBUG, "Found nice chunk %p\n", cur_ch);
 	if (cur_ch != NULL) LOG(DEBUG, "Closest chunk is offset %lld, size %lld\n", cur_ch -> offset, cur_ch -> length);
 	if (cur_ch == NULL ||
-	    cur_ch -> length < poffset - cur_ch -> offset ||
-	    cur_ch -> length < plength ) {
+	    cur_ch -> length < offset - cur_ch -> offset ||
+	    cur_ch -> length < offset + length ) {
 		LOG(DEBUG, "No chunk found - making new one of size %d\n", plength);
 		struct chunk *new_chunk = chunk_manager_get_av_chunk_from_pool(cm);
 		if (new_chunk == NULL)
@@ -127,7 +127,7 @@ ssize_t chunk_manager_gen_chunk (struct chunk_manager *cm, off_t offset, size_t 
 		*chunk_offset = offset - new_chunk -> offset;
 		return new_chunk -> length - offset + new_chunk -> offset;
 	}else{
-		LOG(DEBUG, "Rbtree lookup success!, %ld %ld %ld\n", cur_ch -> length, poffset - cur_ch -> offset, plength);
+		LOG(DEBUG, "Rbtree lookup success!, %ld %ld %ld\n", cur_ch -> length, offset - cur_ch -> offset, length);
 		*chunk_offset = offset - cur_ch -> offset;
 		*ret_ch = cur_ch;
 		return cur_ch -> length - offset + cur_ch -> offset;
