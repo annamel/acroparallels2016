2fe57d7952525f1fb50239ac672eba25001e2480
Quashqwart <CAMAPAOJIEKCA@gmail.com>
2016-05-17 20:18:43 +0300

    fixed some more

--- osamara/mapped_file/mapped_file.c
+++ osamara/mapped_file/mapped_file.c
@@ -10,23 +10,26 @@ extern "C"
 
 #define MF_MAP_FAILED NULL
 uint64_t min (uint64_t a, uint64_t b){
-	return a>b?b:a;
+	return a>=b?b:a;
 }
 uint64_t max (uint64_t a, uint64_t b){
-	return a<b?b:a;
+	return a<=b?b:a;
 }
 mf_handle_t mf_open(const char* pathname){
-	 handle_t * mf = ( handle_t*) malloc(sizeof( handle_t));
+	handle_t * mf = ( handle_t*) malloc(sizeof( handle_t));
 	if (!mf){
 		//printf("Alloc fail\n");
+		//fflush(stdout);
 		errno = ENOMEM;
 		return NULL;
 	}
 	if ((mf->fd = open(pathname, O_RDWR, 0)) == -1){
-		//printf("Open fail\n");
+		//printf("Open fail\n");	
 		free (mf);
 		return NULL;
 	}
+	//printf("Open success\n");
+	//fflush(stdout);
 	mf->mapped = 0;
 	mf->r_map = ( mapmem_handle_t *) malloc(sizeof( mapmem_handle_t));
 	mf->r_map->offset = 0;
@@ -36,6 +39,7 @@ mf_handle_t mf_open(const char* pathname){
 	
 	struct stat st;
 	fstat(mf->fd, &st);
+	mf->fsize = st.st_size;
 	mf->r_map->true_ptr = mmap(NULL, st.st_size, PROT_READ|PROT_WRITE, MAP_SHARED, mf->fd, 0);
 	if (mf->r_map->true_ptr != MAP_FAILED){
 		mf->r_map->size = st.st_size;
@@ -50,7 +54,9 @@ mf_handle_t mf_open(const char* pathname){
 }
 	
 int mf_close(mf_handle_t mf){
-	 handle_t * my_mf = ( handle_t *) mf;
+	if (!mf) return 0;
+	handle_t * my_mf = ( handle_t *) mf;
+	if (my_mf->fd == -1) return 0;
 	close (my_mf->fd);
 	free(my_mf->r_map);
 	free(my_mf);
@@ -58,16 +64,26 @@ int mf_close(mf_handle_t mf){
 }
 // mapmem_handle_t *  mf->r_map = NULL;
 ssize_t mf_read(mf_handle_t mf, void* buf, size_t size, off_t offset){
+	if (!mf) return 0;
+	if (((handle_t*)mf)->fd == -1) return 0;
 	mf_mapmem_handle_t mapped_mem = NULL;
-
+	handle_t * my_mf = (handle_t*) mf;
 	void* buff = mf_map(mf, offset, size, &mapped_mem);
-
+	mapmem_handle_t* mm = (mapmem_handle_t*)mapped_mem;
+	//printf("or: size:%d\n offset: %d fsize: %d mapsize: %d", size,offset ,my_mf->fsize);
+	size = min(my_mf->fsize-offset, min(mm->size, size));
+	
+	//printf("%d\n", size);
 	memcpy(buf, buff, size);
+	
 	mf_unmap(mf, mapped_mem);
-	return (ssize_t) size;
+	// if (offset + size > filesize) return max(0, filesize - offset); else return size        ---------offset---filesize---offset+size
+	return (ssize_t) size; // min(size, max(0, filesize - (offset + size)))
 }
 
 ssize_t mf_write(mf_handle_t mf, const void* buf, size_t size, off_t offset){
+	if (!mf) return 0;
+	if (((handle_t*)mf)->fd == -1) return 0;
 	mf_mapmem_handle_t mapped_mem;
 	void* buff = mf_map(mf, offset, size, &mapped_mem);
 	memcpy(buff, buf, size);
@@ -76,7 +92,8 @@ ssize_t mf_write(mf_handle_t mf, const void* buf, size_t size, off_t offset){
 }
 
 void* mf_map(mf_handle_t mf, off_t offset, size_t size, mf_mapmem_handle_t* mapmem_handle){
-	
+	if (!mf) return 0;
+	if (((handle_t*)mf)->fd == -1) return 0;
 	handle_t* my_mf = (handle_t*) mf;
 	//printf("Mappin %d %d; mapped %d %d\n", (int)offset, (int)size, (int)my_mf->r_map->offset, (int)my_mf->r_map->size);
 	*mapmem_handle = (void*)  my_mf->r_map;
@@ -87,7 +104,7 @@ void* mf_map(mf_handle_t mf, off_t offset, size_t size, mf_mapmem_handle_t* mapm
 	long long int pg_size=sysconf(_SC_PAGE_SIZE);
 	
 	long long int moffset = (offset/pg_size)*pg_size;
-	long long int msize = min(max(size+offset%pg_size,pg_size*128*1024), mf_file_size(mf));
+	long long int msize = min(max(size+offset%pg_size,pg_size*128*1024), mf_file_size(mf)-moffset);
 	if (my_mf->r_map->offset <= moffset && my_mf->r_map->size + my_mf->r_map->offset >= offset + size){
 		//printf("yay\n");
 		my_mf->r_map->ptr = (void*)((char*) my_mf->r_map->ptr + (moffset - my_mf->r_map->offset));
@@ -111,6 +128,8 @@ void* mf_map(mf_handle_t mf, off_t offset, size_t size, mf_mapmem_handle_t* mapm
 }
 
 int mf_unmap(mf_handle_t mf, mf_mapmem_handle_t mapmem_handle){
+	if (!mf) return 0;
+	if (((handle_t*)mf)->fd == -1) return 0;
 	if (!mapmem_handle)
 		return -1;
 	 mapmem_handle_t* map_mem_handle = ( mapmem_handle_t*) mapmem_handle;
@@ -124,8 +143,14 @@ int mf_unmap(mf_handle_t mf, mf_mapmem_handle_t mapmem_handle){
 }
 
 ssize_t mf_file_size(mf_handle_t mf){
+	if (!mf) return 0;
+	//printf("HOY");fflush(stdout);
+	handle_t * my_mf = ( handle_t *) mf;
+	//printf("AHOY");fflush(stdout);
+	//printf("file %d\n", my_mf->fd);fflush(stdout);
+	if (my_mf->fd == -1) return 0;
 	struct stat buf;
-	 handle_t * my_mf = ( handle_t *) mf;
+	 
 	fstat(my_mf->fd, &buf);
 	return buf.st_size;
 }
