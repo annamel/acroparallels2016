d5b662771536b56a7cac8dd7155e3b8882ab06c9
voddan <dgvodopyan@gmail.com>
2016-05-23 23:23:14 +0300

    implemented one-thread

--- dvodopian/mapped_file/mapperonio/mapped_file.c
+++ dvodopian/mapped_file/mapperonio/mapped_file.c
@@ -7,6 +7,7 @@
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <memory.h>
+#include <sys/mman.h>
 
 #include "mapped_file.h"
 
@@ -17,35 +18,35 @@ typedef struct {
     int             file_descriptor;
     off_t           file_size;
 
-
-    int             page_num;
-
-//    key_set*        loaded_pages;  // index -> chman_page*
-
-} handle;
+    void* mem;
+} file_handle;
 
 
-typedef struct {
-    void * ptr;
+int map(file_handle * hp) ;
+int unmap(file_handle * hp) ;
 
-    int slice_num;
-} map_handle;
 
 /*
  * Returns NULL on failure.
  */
 mf_handle_t mf_open(const char *pathname) {
-    handle* hp = malloc(sizeof(handle));
+    int fd = open(pathname, O_RDWR);
+    if(-1 == fd) return NULL;
+
+    file_handle* hp = malloc(sizeof(file_handle));
+    if(!hp) return NULL;  // close
+
     hp->sys_page_size = sysconf(_SC_PAGE_SIZE);
+    if(hp->sys_page_size <= 0) return NULL;  // close
 
     hp->file_name = pathname;
-    hp->file_descriptor = open(pathname, O_RDWR);
+    hp->file_descriptor = fd;
 
     struct stat file_stat;
-    fstat(hp->file_descriptor, &file_stat);
+    fstat(fd, &file_stat);
     hp->file_size = file_stat.st_size;
-    hp->page_num =  (int) hp->file_size / hp->sys_page_size;
 
+    hp->mem = NULL;
 
     return hp;
 }
@@ -54,40 +55,79 @@ mf_handle_t mf_open(const char *pathname) {
  * Returns 0 on success and -1 on failure
  */
 int mf_close(mf_handle_t mf) {
-    handle* hp = (handle*) mf;
+    file_handle* hp = (file_handle*) mf;
+    if(!hp) return -1;
 
-    close(hp->file_descriptor);
+    int r1 = unmap(hp);
+    int r2 = close(hp->file_descriptor);
 
-
-    free(mf);
-    return 0;
+    free(hp);
+    return (0 == r1 && 0 == r2) ? 0 : -1;
 }
 
 /*
  * Returns -1 on failure
  */
 ssize_t mf_read(mf_handle_t mf, void* buf, size_t count, off_t offset) {
-    return 0;
+    file_handle* hp = (file_handle*) mf;
+    if(!hp) return -1;
+    if(offset < 0) return -1;  // wtf?
+
+    if(offset + count > hp->file_size) return -1;
+
+
+    if(map(hp)) return -1;
+
+    memcpy(buf, hp->mem + offset, count);
+
+    return count;
 }
 
 /*
  * Returns -1 on failure
  */
 ssize_t mf_write(mf_handle_t mf, const void* buf, size_t count, off_t offset) {
-    return 0;
+    file_handle* hp = (file_handle*) mf;
+    if(!hp) return -1;
+    if(offset < 0) return -1;  // wtf?
+
+    if(offset + count > hp->file_size) return -1;
+
+
+    if(map(hp)) return -1;
+
+    memcpy(hp->mem + offset, buf, count);
+
+    return count;
 }
 
 /*
  * Returns NULL on failure
  */
 void *mf_map(mf_handle_t mf, off_t offset, size_t size, mf_mapmem_handle_t *mapmem_handle) {
-    return NULL;
+    file_handle* hp = (file_handle*) mf;
+    if(!hp) return NULL;
+    if(offset < 0) NULL;  // wtf?
+
+    if(offset + size > hp->file_size) return NULL;
+
+
+    if(map(hp)) return NULL;
+
+    *mapmem_handle = NULL;
+
+    return hp->mem + offset;
 }
 
 /*
  * Returns 0 on success and -1 on failure
  */
 int mf_unmap(mf_handle_t mf, mf_mapmem_handle_t mapmem_handle) {
+    file_handle* hp = (file_handle*) mf;
+    if(!hp) return -1;
+
+    // DO NOTHING
+
     return 0;
 }
 
@@ -95,5 +135,32 @@ int mf_unmap(mf_handle_t mf, mf_mapmem_handle_t mapmem_handle) {
  * Returns -1 on failure
  */
 off_t mf_file_size(mf_handle_t mf) {
-    return 0;
+    file_handle* hp = (file_handle*) mf;
+    if(!hp) return -1;
+
+    return hp->file_size;
+}
+
+/////////////
+// PRIVATE //
+/////////////
+
+/* it's ok to have hp->mem in the wrong state */
+
+/* if failed returns -1, if success returns 0 */
+
+int map(file_handle * hp) {
+    if(hp->mem) return 0;
+    hp->mem = mmap(NULL, (size_t) hp->file_size, PROT_WRITE, MAP_SHARED, hp->file_descriptor, 0);
+
+    return (MAP_FAILED != hp->mem) ? 0 : -1;
+}
+
+int unmap(file_handle * hp) {
+    if(!hp->mem) return 0;
+
+    int r = munmap(hp->mem, (size_t) hp->file_size);
+    hp->mem = NULL;
+
+    return r;
 }
--- dvodopian/mapped_file/test.sh
+++ dvodopian/mapped_file/test.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
-ROOT_LIB_DIR="." ROOT_TEST_DIR="../../dkopyrin ../../ivanov" ../../tester/tester.sh
+#ROOT_LIB_DIR=".." ROOT_TEST_DIR="../../dkopyrin ../../ivanov" ../../tester/tester.sh
+
+cd ../../tester
+pwd
+ROOT_LIB_DIR="../dvodopian" ROOT_TEST_DIR="../dkopyrin ../aivanov ../aneganov" ./tester.sh
